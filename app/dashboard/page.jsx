5ce1ab3aa987336588232aa2c732ad7b40c2a9ab
horidianx69 <ayush.shuklaps@gmail.com>
2025-02-11 12:56:49 +0530

    did signIn page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,12 @@
 
+import { UserButton } from '@clerk/nextjs';
 import React from 'react';
 
 const Dashboard = () => {
   return (
     <div>
       Dashboard
+      <UserButton />
     </div>
   );
 }
